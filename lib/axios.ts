0b048b5c0e761ffdd8e1d7a5464ea1fe845ddc0f
levinh2006b <ledinhvinh7a2@gmail.com>
2026-04-09 15:18:22 +0700

    fix: axios

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,13 +1,10 @@
-// Khi ấn Login thì cần gửi data đi để verify, axios là thư viện giúp gửi data đi
-//axios là thư viện phụ trách gửi api và file này cấu hình api là chỉ gửi dạng JSON để mỗi lần gửi nó không phải dặn lại là tôi gửi dạng JSON 
-
-
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-    headers: {
-        'Content-Type': 'application/json',
-    },
+  headers: {
+    "Content-Type": "application/json",
+  },
+  withCredentials: true,
 });
 
 export default api;
